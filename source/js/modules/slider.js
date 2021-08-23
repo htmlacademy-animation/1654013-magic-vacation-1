d3e5924f3a3fd7bd9cd716aaed9069c94d0b2327
nikita <nikita.krasavtsev.17@gmail.com>
2021-08-23 19:34:23 +0300

    * Анимация главного меню * Анимация меню соцсетей * Анимация блока "Отгадай место первым..." * Анимация текстов в разделе истории * Анимация появления текстового поля ввода на экране "Игра"

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,9 +1,24 @@
 import Swiper from "swiper";
 
-export default () => {
+export default (eventEmitter) => {
+  const activeBackClass = `slider__item--active-back`;
+  let screenChangeHandler;
   let storySlider;
   let sliderContainer = document.getElementById(`story`);
   sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
+
+  const slider = sliderContainer.querySelector(`.slider`);
+  const storySliderModifier1 = `slider--story-page-1`;
+  const storySliderModifier2 = `slider--story-page-2`;
+  const storySliderModifier3 = `slider--story-page-3`;
+  const storySliderModifier4 = `slider--story-page-4`;
+
+  const storySliderModifiers = [
+    storySliderModifier1,
+    storySliderModifier2,
+    storySliderModifier3,
+    storySliderModifier4
+  ];
 
   const setSlider = function () {
     if (((window.innerWidth / window.innerHeight) < 1) || window.innerWidth < 769) {
@@ -26,13 +41,23 @@ export default () => {
             } else if (storySlider.activeIndex === 6 || storySlider.activeIndex === 7) {
               sliderContainer.style.backgroundImage = `url("img/slide4.jpg"), linear-gradient(180deg, rgba(45, 39, 63, 0) 0%, #2F2A42 16.85%)`;
             }
+
+            Array.from(document.querySelectorAll(`.${activeBackClass}`))
+              .forEach((element) => element.classList.remove(activeBackClass));
+
+            if (storySlider.activeIndex < storySlider.previousIndex) {
+              storySlider.slides[storySlider.activeIndex].classList.add(activeBackClass);
+            }
+
+            eventEmitter.emit(`STORY_PAGE_CHANGE`, storySlider.activeIndex);
           },
           resize: () => {
             storySlider.update();
           }
         },
         observer: true,
-        observeParents: true
+        observeParents: true,
+        speed: 1
       });
     } else {
       storySlider = new Swiper(`.js-slider`, {
@@ -51,24 +76,39 @@ export default () => {
         },
         on: {
           slideChange: () => {
+            slider.classList.remove(...storySliderModifiers);
             if (storySlider.activeIndex === 0) {
               sliderContainer.style.backgroundImage = `url("img/slide1.jpg")`;
+              slider.classList.add(storySliderModifier1);
             } else if (storySlider.activeIndex === 2) {
               sliderContainer.style.backgroundImage = `url("img/slide2.jpg")`;
+              slider.classList.add(storySliderModifier2);
             } else if (storySlider.activeIndex === 4) {
               sliderContainer.style.backgroundImage = `url("img/slide3.jpg")`;
+              slider.classList.add(storySliderModifier3);
             } else if (storySlider.activeIndex === 6) {
               sliderContainer.style.backgroundImage = `url("img/slide4.jpg")`;
+              slider.classList.add(storySliderModifier4);
             }
+
+            eventEmitter.emit(`STORY_PAGE_CHANGE`, storySlider.activeIndex);
           },
           resize: () => {
             storySlider.update();
           }
         },
         observer: true,
-        observeParents: true
+        observeParents: true,
+        speed: 1
       });
     }
+
+    eventEmitter.off(`SCREEN_CHANGED`, screenChangeHandler);
+    screenChangeHandler = (event) => {
+      const pageIndex = event.screenName === `story` ? storySlider.activeIndex : null;
+      eventEmitter.emit(`STORY_PAGE_CHANGE`, pageIndex);
+    };
+    eventEmitter.on(`SCREEN_CHANGED`, screenChangeHandler);
   };
 
   window.addEventListener(`resize`, function () {
--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,5 @@
 // modules
+import EventEmitter from './modules/event-emitter';
 import mobileHeight from './modules/mobile-height-adjust.js';
 import slider from './modules/slider.js';
 import menu from './modules/menu.js';
@@ -9,15 +10,17 @@ import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
 
+const eventEmitter = new EventEmitter();
+
 // init modules
 mobileHeight();
-slider();
-menu();
+slider(eventEmitter);
+menu(eventEmitter);
 footer();
 chat();
 result();
 form();
-social();
+social(eventEmitter);
 
-const fullPageScroll = new FullPageScroll();
+const fullPageScroll = new FullPageScroll(eventEmitter);
 fullPageScroll.init();
