c86e3254b7d6effeb7629605dd587c9d766a87e0
nikita <nikita.krasavtsev.17@gmail.com>
2021-11-17 23:41:50 +0300

    Анимация главного приза

--- source/js/modules/page-prizes.js
+++ source/js/modules/page-prizes.js
@@ -1,6 +1,6 @@
 import {animateChars, getCharAnimation} from './char-animation';
 
-export default () => {
+export default (eventEmitter) => {
   const title = document.querySelector(`.prizes__title`);
   const animationShifts = [
     0.18,
@@ -13,4 +13,17 @@ export default () => {
   ];
   animateChars(title, animationShifts.map((shift) =>
     getCharAnimation({duration: 0.4, delay: shift})));
+
+  const loadPrizeImages = () => {
+    const prizeImages = Array.from(document.querySelectorAll(`.prizes__icon img`));
+    prizeImages.forEach((prizeImage) => {
+      prizeImage.setAttribute(`src`, prizeImage.dataset.src);
+    });
+  };
+  const screenChangeHandler = (event) => {
+    if (event.screenName === `prizes`) {
+      loadPrizeImages();
+    }
+  };
+  eventEmitter.on(`SCREEN_CHANGED`, screenChangeHandler);
 };
